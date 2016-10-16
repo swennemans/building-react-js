b03ee2a073b30ed56f2f021df98a52236efd6f30
sven roeterdink <sven.roeterdink1@gmail.com>
2016-10-16 16:52:21 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require("webpack");
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    devtool: 'eval-source-map',
+    path: './',
+    fileName: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [{
+            test: /\.js/,
+            exclude: /node_modules/,
+            loader: 'babel'
+      }],
+  },
+  devServer: {
+        inline: true,
+        port: 4000
+    },
+}
